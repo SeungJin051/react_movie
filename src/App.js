966b3d7f36a44b2c6b923087817ec3971c43e3de
SeungJin051 <83889135+SeungJin051@users.noreply.github.com>
2024-03-07 01:14:39 +0900

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import Button from "./Button";
 import styles from "./App.module.css";
+import { useState, useEffect } from "react";
+
 function App() {
+  const [counter, setCounter] = useState(0);
+  const onClick = () => setCounter(counter + 1);
+  console.log("항상 실행");
+  useEffect(() => {
+    console.log("API 호출");
+  }, []);
   return (
     <div>
-      <h1 className={styles.title}>Welcome</h1>
-      <Button text={"TEST"} />
+      <h1 className={styles.title}>{counter}</h1>
+      <button onClick={onClick}>Click me</button>
     </div>
   );
 }
