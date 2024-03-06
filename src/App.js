32a200248448544cf2ddde5c7bc5fdaeeb22b6c3
SeungJin051 <83889135+SeungJin051@users.noreply.github.com>
2024-03-07 01:31:38 +0900

    Deps

--- src/App.js
+++ src/App.js
@@ -3,13 +3,31 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setCounter(counter + 1);
-  console.log("항상 실행");
+  const onChange = (event) => {
+    setKeyword(event.target.value);
+  };
   useEffect(() => {
-    console.log("API 호출");
+    console.log("한번 실행");
   }, []);
+  useEffect(() => {
+    console.log("keyword 바뀌면 실행");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("counter 바뀌면 실행");
+  }, [counter]);
+  useEffect(() => {
+    console.log("keyword & counter 바뀌면 실행");
+  }, [keyword, counter]);
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="검색..."
+      />
       <h1 className={styles.title}>{counter}</h1>
       <button onClick={onClick}>Click me</button>
     </div>
