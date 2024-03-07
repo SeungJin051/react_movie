acf0c397a42ed85496942e7b086b7dd5f0a690ce
SeungJin051 <83889135+SeungJin051@users.noreply.github.com>
2024-03-07 12:34:46 +0900

    todo list 생성
    삭제 기능 만들기

--- src/App.js
+++ src/App.js
@@ -1,21 +1,42 @@
+import { useState } from "react";
 import styles from "./App.module.css";
-import { useState, useEffect } from "react";
-
-function Hello() {
-  useEffect(() => {
-    console.log("Created :)");
-    return () => console.log("Destoryed :(");
-  }, []);
-  return <h1>Hello</h1>;
-}
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [toDo, setToDo] = useState(""); // 입력된 할 일을 저장하는 상태
+  const [toDos, setToDos] = useState([]); // 할 일 목록을 저장하는 상태
+
+  // 입력된 값을 상태에 업데이트하는 함수
+  const onChange = (event) => setToDo(event.target.value);
+
+  // 할 일을 추가하는 함수
+  const onSubmit = (event) => {
+    event.preventDefault(); // 폼의 기본 동작 방지
+    if (toDo.trim() === "") return; // 입력된 값이 없으면 함수 종료
+
+    // 이전 할 일 목록에 새로운 할 일을 추가하여 상태 업데이트
+    setToDos((prevToDos) => [toDo, ...prevToDos]);
+    setToDo(""); // 입력 필드 초기화
+  };
+  console.log(toDos);
+
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1 className={styles.title}>My Todo List ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="할 일을 적으세요"
+          value={toDo}
+          onChange={onChange}
+        />
+        <button type="submit">추가</button>
+        <hr />
+        <ul>
+          {toDos.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+      </form>
     </div>
   );
 }
