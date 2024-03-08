f78ccb9e88f4560323f9d48660201a13837ca43a
SeungJin051 <83889135+SeungJin051@users.noreply.github.com>
2024-03-08 21:44:46 +0900

    Publishing

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
-import Nav from "./components/Nav";
 
 // tailwind
 import "./index.css";
